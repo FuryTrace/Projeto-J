2c2668e71ac019a764964797c977b8308f99417c
FuryTrace <william11cesar@gmail.com>
2025-11-11 11:14:25 -0300

    Adiciona arquivos de configuração e estrutura do banco de dados para o projeto

---
+++ database-lista_tarefa.sql
@@ -0,0 +1,38 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "-03:00";
+
+--
+-- Banco de dados: `lista_tarefa`
+--
+
+CREATE DATABASE IF NOT EXISTS `lista_tarefa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `lista_tarefa`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tarefas`
+--
+
+CREATE TABLE IF NOT EXISTS `tarefas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` char(120) NOT NULL,
+  `descricao` text NOT NULL,
+  `data_criacao` date NOT NULL,
+  `data_conclusao` date DEFAULT NULL,
+  `status` enum('pendente','em andamento','concluida') NOT NULL DEFAULT 'pendente',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tarefas`
+--
+
+INSERT INTO `tarefas` ( `nome`, `descricao`, `data_criacao`, `data_conclusao`, `status`) VALUES
+('Maria Alves', 'Descrição da tarefa 1', '2020-01-01', '2020-01-02', 'concluida'),
+('Joao Paulo', 'Descrição da tarefa 2', '2025-04-11', '2025-04-12', 'em andamento'),
+('Joana Silveira', 'Descrição da tarefa 3', '2025-07-11', '2025-09-12', 'em andamento'),
+('Ana Clara', 'Descrição da tarefa 4', '2025-04-11', NULL, 'pendente');
+COMMIT;
