407a38e044ce8dc74fafd4ba9deec10b647ccc6c
FuryTrace <william11cesar@gmail.com>
2025-11-16 23:55:11 -0300

    chore: remove integration-test.js

--- src/db/seeds/exemplo_tarefas.js
+++
@@ -1,9 +0,0 @@
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('tarefas').del();
-  await knex('tarefas').insert([
-    { nome: 'Compra', descricao: 'Comprar leite', data_criacao: '2025-11-01', data_conclusao: null, status: 'pendente' },
-    { nome: 'Estudo', descricao: 'Estudar para a prova', data_criacao: '2025-11-02', data_conclusao: null, status: 'pendente' },
-    { nome: 'Ligação', descricao: 'Ligar para o cliente', data_criacao: '2025-11-03', data_conclusao: '2025-11-04', status: 'concluida' }
-  ]);
-};
